cb4e3257aeffbc43c216bfa405c79d5c626639aa
Simeon03 <bozhilovsimeon@gmail.com>
2024-01-21 22:09:16 +0000

    Added gradient

--- app/src/main/java/com/example/jetweather/MainActivity.kt
+++ app/src/main/java/com/example/jetweather/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.jetweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import com.example.jetweather.ui.theme.Gradient1
+import com.example.jetweather.ui.theme.Gradient2
+import com.example.jetweather.ui.theme.Gradient3
+import com.example.jetweather.ui.theme.Gradient4
+import com.example.jetweather.ui.theme.Gradient5
 import com.example.jetweather.ui.theme.JetWeatherTheme
 import com.example.jetweather.views.CurrentWeatherView
 
@@ -22,11 +32,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetWeatherTheme {
                 viewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
-                LazyColumn(
-                    modifier = Modifier.padding(20.dp)
+
+                val gradient = Brush.verticalGradient(
+                    colors = listOf(Gradient1, Gradient2, Gradient3, Gradient4, Gradient5),
+                    startY = 0f,
+                    endY = Float.POSITIVE_INFINITY
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(gradient)
+                        .systemBarsPadding()
                 ) {
-                    item {
-                        CurrentWeatherView(viewModel = viewModel)
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(20.dp)
+                    ) {
+                        item {
+                            CurrentWeatherView(viewModel = viewModel)
+                        }
                     }
                 }
             }
