39905d5306e16465bef00637d3c8fd2943b72448
Simeon03 <bozhilovsimeon@gmail.com>
2024-01-22 21:08:12 +0000

    Card Formatting

--- app/src/main/java/com/example/jetweather/views/WeeklyWeatherView.kt
+++ app/src/main/java/com/example/jetweather/views/WeeklyWeatherView.kt
@@ -3,9 +3,12 @@ package com.example.jetweather.views
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,6 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.jetweather.WeatherViewModel
 import com.example.jetweather.data.WeeklyWeather
 import com.example.jetweather.ui.theme.Typography
@@ -30,18 +35,13 @@ fun WeeklyWeatherView(viewModel: WeatherViewModel) {
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.LightGray),
-    ) {
-
-        for (i in 0..6) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Gray)
-            ) {
+    Card {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            for (i in 0..6) {
                 DailyWeatherView(
                     minTemp = viewModel.fetchDailyMinTemperature(weeklyWeather, i),
                     maxTemp = viewModel.fetchDailyMaxTemperature(weeklyWeather, i),
@@ -51,17 +51,32 @@ fun WeeklyWeatherView(viewModel: WeatherViewModel) {
             }
         }
     }
+
 }
 
 @Composable
 fun DailyWeatherView(minTemp: Int, maxTemp: Int, date: String, weatherCode: String) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Gray),
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text(text = date, style = Typography.bodyLarge)
-        Text(text = weatherCode, style = Typography.bodyMedium)
-        Text(text = "$minTemp/$maxTemp", style = Typography.bodyLarge)
+        Text(
+            text = date,
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Start,
+            style = Typography.bodyLarge
+        )
+        Text(
+            text = weatherCode,
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Start,
+            style = Typography.bodyMedium
+        )
+        Text(
+            text = "$minTemp/$maxTemp",
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Start,
+            style = Typography.bodyLarge
+        )
     }
 }
