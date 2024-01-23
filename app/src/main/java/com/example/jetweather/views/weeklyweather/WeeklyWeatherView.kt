8cd9681e8f21757a80176f52f4176a374ad20ef1
Simeon03 <bozhilovsimeon@gmail.com>
2024-01-23 20:52:09 +0000

    Formatted card

--- app/src/main/java/com/example/jetweather/views/weeklyweather/WeeklyWeatherView.kt
+++ app/src/main/java/com/example/jetweather/views/weeklyweather/WeeklyWeatherView.kt
@@ -2,13 +2,13 @@ package com.example.jetweather.views.weeklyweather
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,11 +16,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
-import com.example.jetweather.viewmodel.WeatherViewModel
 import com.example.jetweather.data.WeeklyWeather
-import com.example.jetweather.ui.theme.Typography
+import com.example.jetweather.ui.theme.Gradient1
+import com.example.jetweather.ui.theme.Gradient2
+import com.example.jetweather.viewmodel.WeatherViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -33,14 +34,24 @@ fun WeeklyWeatherView(viewModel: WeatherViewModel) {
         }
     }
 
-    Card {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(4.dp)
-        ) {
-            for (i in 0..6) {
-                WeeklyWeatherStats(viewModel = viewModel, weeklyWeather = weeklyWeather, index = i)
+    val gradientColors = listOf(
+        Gradient1.copy(alpha = 0.8f),
+        Gradient2.copy(alpha = 0.6f)
+    )
+
+    Card(
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().background(Brush.verticalGradient(gradientColors))) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+            ) {
+                for (i in 0..6) {
+                    WeeklyWeatherStats(viewModel = viewModel, weeklyWeather = weeklyWeather, index = i)
+                }
             }
         }
     }
