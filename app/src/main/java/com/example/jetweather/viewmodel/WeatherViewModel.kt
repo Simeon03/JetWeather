c689e1774de9b837107bc78cb008b16bce265b8f
Simeon03 <bozhilovsimeon@gmail.com>
2024-02-11 21:32:01 +0000

    Refactor WeatherData

--- app/src/main/java/com/example/jetweather/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/jetweather/viewmodel/WeatherViewModel.kt
@@ -2,55 +2,37 @@ package com.example.jetweather.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.jetweather.constants.PlaceholderConstants.FLOAT_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.INT_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.LIST_FLOAT_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.LIST_FULL_DATE_TIME_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.LIST_INT_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.LIST_STRING_24_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.LIST_STRING_PLACEHOLDER
-import com.example.jetweather.constants.PlaceholderConstants.STRING_PLACEHOLDER
+import com.example.jetweather.constants.PlaceholderConstants.FLOAT
+import com.example.jetweather.constants.PlaceholderConstants.INT
+import com.example.jetweather.constants.PlaceholderConstants.LIST_FLOAT
+import com.example.jetweather.constants.PlaceholderConstants.LIST_INT
+import com.example.jetweather.constants.PlaceholderConstants.LIST_STRING
+import com.example.jetweather.constants.PlaceholderConstants.STRING
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 data class WeatherData(
-    val currentTemp: Float,
-    val location: String,
-    val weatherStatus: Int?,
-    val currentMinTemp: Float,
-    val currentMaxTemp: Float,
-    val weeklyMinTemp: List<Float>,
-    val weeklyMaxTemp: List<Float>,
-    val dayOfWeek: List<String>,
-    val weeklyWeatherCode: List<Int>,
-    val hourlyTemperature: List<Float>,
-    val hourlyTime: List<String>,
-    val hourlyWeatherStatus: List<Int>,
-    val sunriseTime: List<String>,
-    val sunsetTime: List<String>,
-    val hourlyHumidity: List<Int>,
+    val currentTemp: Float = FLOAT,
+    val location: String = STRING,
+    val weatherStatus: Int? = INT,
+    val currentMinTemp: Float = FLOAT,
+    val currentMaxTemp: Float = FLOAT,
+    val weeklyMinTemp: List<Float> = LIST_FLOAT,
+    val weeklyMaxTemp: List<Float> = LIST_FLOAT,
+    val dayOfWeek: List<String> = LIST_STRING,
+    val weeklyWeatherCode: List<Int> = LIST_INT,
+    val hourlyTemperature: List<Float> = LIST_FLOAT,
+    val hourlyTime: List<String> = LIST_STRING,
+    val hourlyWeatherStatus: List<Int> = LIST_INT,
+    val sunriseTime: List<String> = LIST_STRING,
+    val sunsetTime: List<String> = LIST_STRING,
+    val hourlyHumidity: List<Int> = LIST_INT,
 )
 
 class WeatherViewModel(private val repo: WeatherRepository) : ViewModel() {
 
-    var weatherData = MutableStateFlow(WeatherData(
-        FLOAT_PLACEHOLDER,
-        STRING_PLACEHOLDER,
-        INT_PLACEHOLDER,
-        FLOAT_PLACEHOLDER,
-        FLOAT_PLACEHOLDER,
-        LIST_FLOAT_PLACEHOLDER,
-        LIST_FLOAT_PLACEHOLDER,
-        LIST_STRING_PLACEHOLDER,
-        LIST_INT_PLACEHOLDER,
-        LIST_FLOAT_PLACEHOLDER,
-        LIST_STRING_24_PLACEHOLDER,
-        LIST_INT_PLACEHOLDER,
-        LIST_FULL_DATE_TIME_PLACEHOLDER,
-        LIST_FULL_DATE_TIME_PLACEHOLDER,
-        LIST_INT_PLACEHOLDER,
-    ))
+    var weatherData = MutableStateFlow(WeatherData())
     var isLoading = MutableStateFlow(true)
 
     init {
@@ -61,12 +43,12 @@ class WeatherViewModel(private val repo: WeatherRepository) : ViewModel() {
         viewModelScope.launch {
             isLoading.value = true
             try {
-                // Fetch all data and update weatherData state
                 val location = repo.fetchLocation().first()
                 val currentTemp = repo.fetchCurrentTemperature().first()
                 val weatherStatus = repo.fetchCurrentWeatherStatus().first()
                 val currentMinTemp = repo.fetchCurrentMinTemp().first()
                 val currentMaxTemp = repo.fetchCurrentMaxTemp().first()
+
                 val weeklyMinTemp = repo.fetchWeeklyMinTemp().first()
                 val weeklyMaxTemp = repo.fetchWeeklyMaxTemp().first()
                 val dayOfWeek = repo.fetchDayOfWeek().first()
