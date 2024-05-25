ccca8013bb862c1267f3fb4ee2978b050383354b
Simeon03 <bozhilovsimeon@gmail.com>
2024-05-25 21:41:41 +0100

    Add test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,27 +63,33 @@ allprojects {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // Unit Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // Android Instrumentation Testing
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Debug Implementation
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
---
+++ app/src/androidTest/java/com/example/jetweather/CurrentWeatherRepoTestCase.kt
@@ -0,0 +1,32 @@
+package com.example.jetweather
+
+import com.example.jetweather.constants.Api
+import com.example.jetweather.model.OpenMeteo
+import com.example.jetweather.model.RetrofitInstance
+import com.example.jetweather.repos.sub.DefaultCurrentWeatherRepository
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNotSame
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class CurrentWeatherRepoTestCase {
+
+    private lateinit var currentWeatherRepo: DefaultCurrentWeatherRepository
+    private lateinit var weatherApi: OpenMeteo
+
+    @Before
+    fun setUp() {
+        weatherApi = RetrofitInstance.get(Api.OPEN_METEO_BASE_URL).create(OpenMeteo::class.java)
+        currentWeatherRepo = DefaultCurrentWeatherRepository(weatherApi)
+    }
+
+    @Test
+    fun testFetchTemp() = runBlocking {
+        val temp = currentWeatherRepo.fetchTemp().first()
+        assertNotSame(0, temp)
+        assertNotNull(temp)
+    }
+
+}
